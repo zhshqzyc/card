256831db5ccde28c91360fb4f29d0adf80ee60eb
Hui Zhao <zhshqzyc@gmail.com>
2024-05-13 16:07:36 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CardComponent]
 })
 export class AppComponent {
-  title = 'card';
+  title = 'Card';
+  subTitle = '--Demo';
+  iconClasses = '';
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  @Input() public title = '';
+  @Input() public subTitle = '';
+  @Input() public iconClasses = '';
+}
